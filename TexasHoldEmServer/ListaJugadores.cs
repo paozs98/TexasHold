3c8d6a0f478940982de643f06014f6ef3bf838b3
notCharlie28 <carlos.obando.avendana@est.una.ac.cr>
2019-05-07 01:58:15 -0600

    Faltan Implementar 2 Clases para la logica del juego

---
+++ TexasHoldEmServer/ListaJugadores.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldEmServer
+{
+    class ListaJugadores 
+    {
+        List<Jugador> list = new List<Jugador>();
+        public ListaJugadores()
+        {
+        }
+      
+        public void CopyTo(Jugador[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
+        }
+
+        public ListaJugadores(ListaJugadores PlayerList)
+        {
+            this.list.AddRange(PlayerList.list);
+        }
+
+        public void Add(Jugador item)
+        {
+            list.Add(item);
+        }
+
+        public int IndexOf(Jugador item)
+        {
+            return list.IndexOf(item);
+        }
+
+        public void Insert(int index, Jugador item)
+        {
+            list.Insert(index, item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(Jugador item)
+        {
+            return list.Contains(item);
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public IEnumerator<Jugador> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        public bool Remove(Jugador item)
+        {
+            return list.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        public Jugador this[int index]
+        {
+            
+            get
+            {
+                while (index > list.Count() - 1)
+                    index -= list.Count();
+                while (index < 0)
+                    index += list.Count();
+                return list[index];
+            }
+            set
+            {
+                while (index > list.Count() - 1)
+                    index -= list.Count();
+                while (index < 0)
+                    index += list.Count();
+                list[index] = value;
+            }
+        }
+
+        public Jugador GetJugador(ref int index)
+        {
+            while (index > list.Count() - 1)
+                index -= list.Count();
+            while (index < 0)
+                index += list.Count();
+            return list[index];
+        }
+
+        
+
+    }
+}
+
+
--- TexasHoldEmServer/jugador.cs
+++ TexasHoldEmServer/jugador.cs
@@ -8,6 +8,69 @@ namespace TexasHoldEmServer
 {
     class Jugador
     {
+        protected Mano myHand = new Mano();
+        protected string nombre;
+        protected int dineroInicial;
+        protected int apuesta;
+        protected int dineroActual;
+        protected bool mostrarCartas;
+
+
+        public void SetNombre(string n)
+        {
+            nombre = n;
+        }
+        public void setMostrarCartas(bool mo) {
+            mostrarCartas = mo;
+        }
+        public void SetDineroActual(int actual) {
+            dineroActual = actual;
+        }
+
+        public void SetApuesta(int ap) {
+            apuesta = ap;
+        }
+
+        public void SetDineroInicial(int i) {
+            dineroInicial = i;
+        }
+
+        public void SetMano(Mano nue) {
+            myHand = nue;
+        }
+        public int GetDineroInicial() {
+            return dineroInicial;
+        }
+        public bool IsMostrarCartas() {
+            return mostrarCartas;
+        }
+        public int GetDineroActual() {
+            return dineroActual;
+        }
+        public int getApuesta() {
+            return apuesta;
+        }
+        public string GetNombre() {
+            return nombre;
+        }
+        public Mano GetMano() {
+            return myHand;
+        }
+        public void AgregarMano(Mano mano) {
+            myHand += mano;
+        }
+
+        public void AgregarMano(Carta carta) {
+            myHand.Agregar_Carta(carta);
+        }
+
+        public void MostrarCartas() {
+            mostrarCartas = true;
+        }
+
+        
+        
+
         public Jugador()
         {
             try
@@ -49,5 +112,5 @@ namespace TexasHoldEmServer
                 Console.Read();
             }
         }
-    }
-}
+    }//cierre de la clase jugador 
+}//cierre del namespace 
