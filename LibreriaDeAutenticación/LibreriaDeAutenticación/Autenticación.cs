60a1fd3d7e5119a0d3d492dba9c7bc9b750aab41
Roberto Rojas <50005436+Rober2R@users.noreply.github.com>
2019-05-10 11:48:14 -0600

    DLL falta que funcione

---
+++ LibreriaDeAutenticación/LibreriaDeAutenticación/Autenticación.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.DirectoryServices;
+
+
+namespace LibreriaDeAutenticación
+{
+    public class Autenticación
+    {
+        private string path;
+        private string dominio;
+        private string usuario;
+        private string password;
+        private string domUsua;
+
+        public Autenticación(string usu, string pass){
+            path = @"WS16";
+            dominio = @"WS16.local";
+            usuario = usu;
+            password = pass;
+            domUsua = dominio + @"\" + usuario;
+        }
+
+        public bool autetificar()
+        {
+            bool res = false;
+            res = autenticaUsuario(path, domUsua, password);
+            return res;
+        }
+
+        public bool autenticaUsuario(string pat, string domUs, string pass)
+        {
+            DirectoryEntry de = new DirectoryEntry(pat, domUs, pass, AuthenticationType.Security);
+        }
+    }
+}
