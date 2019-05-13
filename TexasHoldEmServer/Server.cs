2f1f357d26b87ad23875166d7566d36c2287d853
paozs98 <38383063+paozs98@users.noreply.github.com>
2019-05-13 16:15:44 -0600

    crea hilos

--- TexasHoldEmServer/Server.cs
+++ TexasHoldEmServer/Server.cs
@@ -10,38 +10,54 @@ namespace TexasHoldEmServer
 {
     class Server
     {
-        public Server()
-        {
-            try
+        private TcpListener _server;
+            private Boolean _isRunning;
+ 
+        public Server(){
+                _server = new TcpListener(IPAddress.Any, 8010);
+                _server.Start();
+                _isRunning = true;
+
+                loopClients();
+                
+        }
+
+        public void LoopClients(){
+            while (_isRunning)
             {
-                TcpListener myList = new TcpListener(IPAddress.Any, 8010);
-                myList.Start();
-
-                Console.WriteLine("Server running at port 8001...");
-                Console.WriteLine("Waiting for a connection...");
-
-                Socket s = myList.AcceptSocket();
-                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-
-                byte[] b = new byte[100];
-                int k = s.Receive(b);
-                Console.WriteLine("Recieved...");
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(b[i]));
-
-                ASCIIEncoding asen = new ASCIIEncoding();
-                s.Send(asen.GetBytes("The string was recieved by the server."));
-                Console.WriteLine("\nSent Acknowledgement");
-                /* clean up */
-                s.Close();
-                Console.Read();
-                myList.Stop();
-
+                // wait for client connection
+                TcpClient newClient = _server.AcceptTcpClient();
+ 
+                // client found.
+                // create a thread to handle communication
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                t.Start(newClient);
             }
-            catch (Exception e)
+        }
+        public void HandleClient(object obj){
+            // retrieve client from parameter passed to thread
+            TcpClient client = (TcpClient)obj;
+ 
+            // sets two streams
+            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+            // you could use the NetworkStream to read and write, 
+            // but there is no forcing flush, even when requested
+ 
+            Boolean bClientConnected = true;
+            String sData = null;
+ 
+            while (bClientConnected)
             {
-                Console.WriteLine("Error: " + e.StackTrace);
-                Console.Read();
+                // reads from stream
+                sData = sReader.ReadLine();
+ 
+                // shows content on the console.
+                Console.WriteLine("Client &gt; " + sData);
+ 
+                // to write something back.
+                // sWriter.WriteLine("Meaningfull things here");
+                // sWriter.Flush();
             }
         }
     }
