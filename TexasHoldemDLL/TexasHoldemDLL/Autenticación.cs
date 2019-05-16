e915536ed987a8bd787b15370c8418952c94c3b3
Roberto Rojas <50005436+Rober2R@users.noreply.github.com>
2019-05-15 21:20:19 -0600

    DLL
    Ya no marca error, pero no sé si funciona ; solo es parte de autentificación de usuarios, no registra nuevos.

---
+++ TexasHoldemDLL/TexasHoldemDLL/Autenticación.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace TexasHoldemDLL
+{
+    public class Autenticación
+    {
+
+        public bool autentificar(string usu, string pass)
+        {
+            string path = @"WS16";
+            string dominio = @"WS16.local";
+            string usuario = usu;
+            string password = pass;
+            string domUsua = dominio + @"\" + usuario;
+
+            DirectoryEntry de = new DirectoryEntry(path, usuario, password, AuthenticationTypes.Secure);
+            try
+            {
+                DirectorySearcher ds = new DirectorySearcher(de);
+                ds.FindOne();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
