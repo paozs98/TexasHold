5ff0af5fa2f391429a0256564a8811931a44ccc4
notcharlie28 <carlos.obando.avendana@est.una.ac.cr>
2019-05-30 13:16:10 -0600

    Imports arreglados

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    class Program
+    class Server
     {
         static ColeccionJugador cj;//Aca tiene que entrar todos los jugadores de la bd 
 
